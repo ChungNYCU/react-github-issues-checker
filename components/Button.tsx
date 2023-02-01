83343ded3f3a9afb8e880000dac1e82eeed36297
Wei-Che Chung <wchung@seattleu.edu>
2023-01-31 23:06:58 -0800

    Create Button

---
+++ components/Button.tsx
@@ -0,0 +1,19 @@
+import { MouseEventHandler } from "react";
+
+type ButtonProps = {
+    children: any;
+    className: string;
+    onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = (props: ButtonProps) => {
+    return (
+        <button
+            className={`p-2 rounded-md hover:ring-2 hover:ring-gray-300 ${props.className}`}
+            onClick={props.onClick}>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button
