6e0dcd418afcb23a524d94777b8ba8d77ddc64b2
Wei-Che Chung <wchung@seattleu.edu>
2023-01-31 23:09:15 -0800

    Create layout

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Footer from '@/sections/Footer'
+import Header from '@/sections/Header'
+
+type LayoutProps = {
+    children: any;
+}
+
+const Layout = (props: LayoutProps) => {
+    return (
+        <>
+            <Head>
+                <title>GitHub issues checker</title>
+                <meta name='description content' content='GitHub issues checker' />
+            </Head>
+
+            <div className='min-h-screen flex flex-col'>
+                <Header />
+                <main className='flex-grow'>
+                    {props.children}
+                </main>
+                <Footer />
+            </div>
+        </>
+    )
+}
+
+export default Layout
