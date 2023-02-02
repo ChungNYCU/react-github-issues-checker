d26e345f5dea74911dafd76e8bd38720a99e2171
Wei-Che Chung <wchung@seattleu.edu>
2023-02-01 22:24:53 -0800

    Create page not found page

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+const PageNotFound = () => {
+    return (
+        <div className="text-center text-gray-600 font-semibold mt-20 text-2xl">Page Not Found</div>
+    )
+}
+
+export default PageNotFound
