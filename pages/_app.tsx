b8de6ec8a466956c26bc4da090971567bf47489d
Wei-Che Chung <wchung@seattleu.edu>
2023-02-02 05:20:41 -0800

    Import SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SessionProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </ThemeProvider>
   )
 }
