870d079fb5a411973a0c3e4196323fed95627bcb
Wei-Che Chung <wchung@seattleu.edu>
2023-02-03 03:37:14 -0800

    Update app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
+import { Provider } from "react-redux";
+
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 
 import Layout from '@/components/Layout'
+import { store } from "@/state";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
       <SessionProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </SessionProvider>
     </ThemeProvider>
   )
