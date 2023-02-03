676196c98975d174adf45b72d7e346dba3b43d16
Wei-Che Chung <wchung@seattleu.edu>
2023-02-03 03:37:33 -0800

    update tmp issue list

--- pages/issueList.tsx
+++ pages/issueList.tsx
@@ -1,75 +1,39 @@
-import { useState, useEffect } from 'react'
+import { MouseEventHandler, useState } from 'react'
+import { useSession } from 'next-auth/react';
+import Card from '@/components/Card';
 
-import Link from 'next/link'
-import { useSession } from 'next-auth/react'
+type IssueListProps = {
+    children: any;
+    className: string;
+    repoName: string;
+    onClick: MouseEventHandler<HTMLButtonElement>;
+}
 
-import Button from '@/components/Button'
-
-//const reposFromLocalStorage = JSON.parse(window.localStorage.getItem('repos')!)
-
-
-const IssueList = () => {
+const IssueList = (props: IssueListProps) => {
 
     const { data: session } = useSession()
     const USERNAME = session?.user?.name
+    const [repoIssues, setRepoIssues] = useState<Object[]>([])
 
-    const [repos, setRepos] = useState<Object[]>([])
-    const [orgs, setOrgs] = useState([])
-    const [isLoading, setLoading] = useState(false)
-
-    const fetchRepos = async () => {
-        setLoading(true)
-        fetch(`https://api.github.com/users/${USERNAME}/repos`)
+    const fetchRepoIssues = async (repoName: string) => {
+        fetch(`https://api.github.com/repos/${USERNAME}/${repoName}/issues`)
             .then(response => response.json())
             .then(data => {
-                setRepos(data)
-                setLoading(false)
+                setRepoIssues(data)
+                console.log(data)
             })
             .catch(error => console.error(error))
     }
-
-    const fetchOrgs = async () => {
-        setLoading(true)
-        fetch(`https://api.github.com/users/${USERNAME}/orgs`)
-            .then(response => response.json())
-            .then(data => {
-                setOrgs(data)
-                setLoading(false)
-            })
-            .catch(error => console.error(error))
-    }
-
-    useEffect(() => {
-        // get user repos
-        fetchRepos()
-        // get org repos
-        fetchOrgs()
-    }, [])
-
-    if (isLoading) return <div>Loading...</div>
-
-    if (session) {
-        return (
-            <section>
-                <h1 className='dark:text-blue-500'>Issue List</h1>
-                {repos.map((repo: any, i) => (
-                    <div key={i}>{repo.name}</div>
+    return (
+        <div>
+            <h1 className='mt-10 dark:text-blue-500'>Issue List</h1>
+            <div className='mt-10 grid grid-cols-3 gap-4 content-start'>
+                {repoIssues.map((issue: any, i) => (
+                    <Card openIssues={0} className='' onClick={() => { }} key={i}>{issue.name}</Card>
                 ))}
-            </section>
-        )
-    } else {
-        return (
-            <section className="flex items-center h-full p-16 dark:text-gray-100">
-                <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-                    <h3 className="text-center font-semibold mt-20 dark:text-blue-500">Please Login</h3>
-                    <Link href="/">
-                        <Button className='ease-in duration-300 mt-20 bg-blue-500 text-white px-6' onClick={() => { }}>Back to homepage</Button>
-                    </Link>
-                </div>
-            </section>
-        )
-    }
-
+            </div>
+        </div>
+    )
 }
 
 export default IssueList
