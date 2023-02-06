c6b08732379dd6b096e558eac7e0002ad7d7208a
Wei-Che Chung <wchung@seattleu.edu>
2023-02-05 23:13:35 -0800

    Delete useless files

--- hooks/useTypedSelector.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from '../state';
-import { useSelector, TypedUseSelectorHook } from "react-redux";
-
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
--- state/store.ts
+++
@@ -1,5 +0,0 @@
-import thunk from "redux-thunk";
-import reducers from "./reducers";
-import { createStore, applyMiddleware } from "redux";
-
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
