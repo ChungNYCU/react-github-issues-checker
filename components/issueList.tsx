96c78255640ac643efe0cc222d9ba14abf3c9610
Wei-Che Chung <wchung@seattleu.edu>
2023-02-05 23:35:26 -0800

    Add detailed comments to the IssueList component

--- components/issueList.tsx
+++ components/issueList.tsx
@@ -14,42 +14,53 @@ type IssueListProps = {
     onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
+// Defining the functional component for the IssueList
 const IssueList = (props: IssueListProps) => {
 
+    // Constants for the number of issues to be displayed per page
     const PER_PAGE = props.per_page
+
+    // State variables to keep track of the page number and issues loaded
     const [pageCount, setPageCount] = useState<number>(1)
     const [page, setPage] = useState<number>(PER_PAGE * pageCount)
     const [repoIssues, setRepoIssues] = useState<Object[]>([])
     const [loadMore, setLoadMore] = useState<boolean>(true)
 
+    // Function to fetch the repository issues from Github API
     const fetchRepoIssues = async () => {
         fetch(`https://api.github.com/repos/${props.username}/${props.reponame}/issues?per_page=${page}`)
             .then(response => response.json())
             .then(data => {
+                // If number of issues received is less than the requested page, it means there are no more issues to be loaded
                 if (data.length < page) {
                     setLoadMore(false)
                 }
+                // Updating the repository issues state with the received data
                 setRepoIssues(data)
                 console.log(`Load page ${pageCount}`, data)
             })
             .catch(error => console.error(error))
     }
 
-    useEffect(() => {
-        fetchRepoIssues()
-    }, [page])
-
-    useEffect(() => {
-        setPage(PER_PAGE * pageCount)
-    }, [pageCount])
-
+    // UseBottomScrollListener hook to detect when the user has scrolled to the bottom of the page
     useBottomScrollListener(() => {
+        // If there are still issues to be loaded, increase the page count to load the next page
         if (loadMore === true) {
             setPageCount(pageCount + 1)
         } else {
             console.log('No more issue')
         }
     })
+
+    // UseEffect hook to set the page number when the page count changes
+    useEffect(() => {
+        setPage(PER_PAGE * pageCount)
+    }, [pageCount])
+
+    // UseEffect hook to fetch the repository issues when the page number changes
+    useEffect(() => {
+        fetchRepoIssues()
+    }, [page])
 
     return (
         <div className={`${props.className} mt-3`}>
