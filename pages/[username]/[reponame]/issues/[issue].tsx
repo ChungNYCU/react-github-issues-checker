5fe441eb4d881b6318cabbdc10de60994b9d4901
Wei-Che Chung <wchung@seattleu.edu>
2023-02-05 23:14:30 -0800

    Create tmp [issue].tsx for issue detail page

--- pages/[username]/[reponame]/issues/[issue].tsx
+++ pages/[username]/[reponame]/issues/[issue].tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
-import { Container } from 'semantic-ui-react';
-import { useActions } from "../../../../hooks/useActions";
-import { useTypedSelector } from "../../../../hooks/useTypedSelector";
+// import { Container } from 'semantic-ui-react';
+// import { useActions } from "../../../../hooks/useActions";
+// import { useTypedSelector } from "../../../../hooks/useTypedSelector";
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import RepositoryMenu from '../../../../components/RepositoryMenu';
-import RepositoryHeader from '../../../../components/RepositoryHeader';
-import RepositoryContent from '../../../../components/RepositoryContent';
+// import RepositoryMenu from '../../../../components/RepositoryMenu';
+// import RepositoryHeader from '../../../../components/RepositoryHeader';
+// import RepositoryContent from '../../../../components/RepositoryContent';
 
 interface IParams extends ParsedUrlQuery {
   username: string;
@@ -15,25 +15,25 @@ interface IParams extends ParsedUrlQuery {
 };
 
 const Repo = ({ username, repo }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { data, error, loading } = useTypedSelector((state) => state.repository);
-  const { description, topics, readme }: { description: string, topics: string[], readme: string } = data;
-  const { GetRepository } = useActions();
+  // const { data, error, loading } = useTypedSelector((state) => state.repository);
+  // const { description, topics, readme }: { description: string, topics: string[], readme: string } = data;
+  // const { GetRepository } = useActions();
   const router = useRouter();
-  useEffect(() => {
-    GetRepository(username, repo);
-  }, []);
+  // useEffect(() => {
+  //   GetRepository(username, repo);
+  // }, []);
 
-  useEffect(() => {
-    if (error != null) router.push('/404');
-  }, [error]);
+  // useEffect(() => {
+  //   if (error != null) router.push('/404');
+  // }, [error]);
 
   return (
     <>
-      <RepositoryMenu />
+      {/* <RepositoryMenu />
       <RepositoryHeader username={username} repo={repo} data={data} />
       <Container>
         <RepositoryContent description={description} topics={topics} readme={readme} />
-      </Container>
+      </Container> */}
     </>
   )
 };
