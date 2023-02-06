12fee548b5923b8ce4d841b0c775952f1b65056b
Wei-Che Chung <wchung@seattleu.edu>
2023-02-05 23:15:56 -0800

    Streamline code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,17 @@
-import { Provider } from "react-redux";
-
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 
 import Layout from '@/components/Layout'
-import { store } from "@/state";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
       <SessionProvider>
-        <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Provider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </SessionProvider>
     </ThemeProvider>
   )
