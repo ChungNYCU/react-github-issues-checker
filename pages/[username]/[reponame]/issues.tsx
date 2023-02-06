ec291ba1a8355ad51b189d49ce32ee47847c4d1a
Wei-Che Chung <wchung@seattleu.edu>
2023-02-05 23:35:16 -0800

    Add detailed comments to the Issues component

--- pages/[username]/[reponame]/issues.tsx
+++ pages/[username]/[reponame]/issues.tsx
@@ -1,16 +1,18 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-
 import { ParsedUrlQuery } from 'querystring'
 
 import IssueList from '@/components/issueList'
 
+// Defining an interface for the URL parameters
 interface IParams extends ParsedUrlQuery {
   username: string;
   reponame: string;
 };
 
+// The component that renders the issues for a particular repository
 const Issues = ({ username, reponame }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 
+  // The number of issues to be displayed per page
   const PER_PAGE = 10
 
   return (
@@ -18,16 +20,24 @@ const Issues = ({ username, reponame }: InferGetServerSidePropsType<typeof getSe
       <div>
         <h1 className='mt-10 dark:text-blue-500'>{`Issue list`}</h1>
         <h3 className='mt-5'>{reponame}</h3>
-        <IssueList username={username} reponame={reponame} per_page={PER_PAGE}
-          className={''} onClick={() => { }} />
+        <IssueList
+          username={username}
+          reponame={reponame}
+          per_page={PER_PAGE}
+          className={''}
+          onClick={() => { }}
+        />
       </div>
     </>
   )
 };
 
+// A function that fetches the data needed for the component 'Issues'
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Extracting the parameters 'username' and 'reponame' from the URL query
   const { username, reponame } = context.query as IParams
 
+  // Returning the extracted parameters as props for the component 'Issues'
   return { props: { username, reponame } }
 };
 
