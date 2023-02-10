742b1cde42df1aad58e0c14706a6203fcc5234bd
Wei-Che Chung <wchung@seattleu.edu>
2023-02-10 08:31:19 -0800

    Add filter feature in issueList

--- components/issueList.tsx
+++ components/issueList.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState, MouseEventHandler } from 'react'
 import { useBottomScrollListener } from 'react-bottom-scroll-listener'
 
 import Link from 'next/link'
-import { ArrowLeftIcon } from '@heroicons/react/20/solid'
+import { ArrowLeftIcon, AdjustmentsHorizontalIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 
 import { fetchRepoIssues, handleBackButtonClick } from './fetchGitHubApi'
 import Button from './Button'
 import IssueCard from './IssueCard'
 import Loading from './Loading'
 import { WorkStatus } from '@/modules/WorkStatus'
+import WorkStatusDropDown from './WorkStatusDropDown'
 
 type IssueListProps = {
     username: string;
@@ -44,7 +45,9 @@ const IssueList = (props: IssueListProps) => {
         return WorkStatus.NoLabel
     }
 
-    //const handleFilterButtonClick = () => { }
+    const handleFilterButtonClick = (label: string) => {
+        setLabels(label)
+    }
 
     // UseBottomScrollListener hook to detect when the user has scrolled to the bottom of the page
     useBottomScrollListener(() => {
@@ -69,7 +72,8 @@ const IssueList = (props: IssueListProps) => {
 
     return (
         <div className={`${props.className} mt-3`}>
-            <div className='mt-10 mb-5 flex flex-row items-center justify-between'>
+
+            <div className='mt-10 flex flex-row items-center justify-between'>
                 <div className='flex items-center justify-left '>
                     <div>
                         <Button
@@ -78,15 +82,39 @@ const IssueList = (props: IssueListProps) => {
                             <ArrowLeftIcon className="h-5 w-5" aria-hidden="true" />
                         </Button>
                     </div>
-                    <Button className='ml-2 bg-blue-300 text-black hover:bg-blue-500 hover:text-white px-6' onClick={() => { }}>Open</Button>
-                    <Button className='ml-2 bg-red-300 text-black hover:bg-red-500 hover:text-white px-6' onClick={() => { }}>In Progress</Button>
-                    <Button className='ml-2 bg-green-300 text-black hover:bg-green-500 hover:text-white px-6' onClick={() => { }}>Done</Button>
+
                 </div>
-                <div className='flex items-center justify-left '>
-                    <Button className='ml-2 bg-gray-300 text-black hover:bg-gray-500 hover:text-white px-6' onClick={() => { }}>Order</Button>
-                    <Button className='ml-2 bg-yellow-300 text-black hover:bg-yellow-500 hover:text-white px-6' onClick={() => { }}>Create Issue</Button>
+                <div className='flex items-center justify-left'>
+                    <Button className='ml-2 bg-yellow-300 text-black hover:bg-yellow-500 hover:text-white px-6 py-2 ' onClick={() => { }}>Create Issue</Button>
                 </div>
             </div>
+
+            <div className='mt-2 mb-5 flex flex-row items-center justify-between'>
+                <div className='flex items-center justify-left'>
+                    <WorkStatusDropDown workStatus={labels === '' ? 'Filter' : labels}
+                        onOpenStatusButtonClick={() => { handleFilterButtonClick(WorkStatus.Open) }}
+                        onInProgressStatusButtonClick={() => { handleFilterButtonClick(WorkStatus.InProgress) }}
+                        onDoneStatusButtonClick={() => { handleFilterButtonClick(WorkStatus.Done) }}
+                    />
+                    <Button
+                        className='ml-2 inline-flex w-full justify-center rounded-md hover:ring-2 hover:ring-gray-300 dark:hover:ring-gray-700 bg-gray-300 px-4 py-2 text-sm font-medium text-black shadow-sm hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2'
+                        onClick={() => { }}>
+                        Order
+                        <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                    </Button>
+                    <Button
+                        className={'ml-2 bg-gray-300 text-black hover:bg-gray-500 hover:text-white px-6 py-2 flex flex-row justify-start items-center w-full text-left text-sm'}
+                        onClick={() => { handleFilterButtonClick('') }}>
+                        <AdjustmentsHorizontalIcon className="h-5 w-5 mr-2" aria-hidden="true" />
+                        Reset 
+                    </Button>
+                </div>
+                <div className='flex items-center justify-left'>
+                    <input className="shadow appearance-none border border-gray-200 dark:border-gray-700 rounded w-full py-1 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline" id="search" type="text" placeholder="Search"></input>
+                    <Button className='ml-2 bg-blue-300 text-black hover:bg-blue-500 hover:text-white px-6' onClick={() => { }}>Search</Button>
+                </div>
+            </div>
+
             {repoIssues.map((issue: any, i: number) => (
                 <Link legacyBehavior href={`/${props.username}/${props.reponame}/issues/${issue.number}`} className='' key={'link' + i}>
                     <div>
