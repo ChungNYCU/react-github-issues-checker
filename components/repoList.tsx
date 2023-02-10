73b37a1f0f45f3db4f79c6bdf598de6e9ef5a373
Wei-Che Chung <wchung@seattleu.edu>
2023-02-10 08:32:09 -0800

    Update visual design in repoList

--- components/repoList.tsx
+++ components/repoList.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState, MouseEventHandler } from 'react'
 
 import Link from 'next/link'
+import { ArrowLeftIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 
 import { fetchRepos, handleBackButtonClick } from './fetchGitHubApi'
 import RepoCard from './RepoCard'
 import Loading from './Loading'
 import Button from './Button'
-import { ArrowLeftIcon } from '@heroicons/react/20/solid'
 
 type RepoListProps = {
     username: string;
@@ -34,21 +34,22 @@ const RepoList = (props: RepoListProps) => {
     return (
         <div className={`${props.className} mt-3 gap-4 content-start`}>
             <div className='mt-10 mb-5 flex flex-row items-center justify-between'>
-                <div className='flex items-center justify-left '>
-                    <div>
-                        <Button
-                            className={'ml-2 bg-gray-300 text-black hover:bg-gray-500 hover:text-white px-2 py-2'}
-                            onClick={handleBackButtonClick}>
-                            <ArrowLeftIcon className="h-5 w-5" aria-hidden="true" />
-                        </Button>
-                    </div>
-                    <Button className='ml-2 bg-blue-300 text-black hover:bg-blue-500 hover:text-white px-6' onClick={() => { }}>Open</Button>
-                    <Button className='ml-2 bg-red-300 text-black hover:bg-red-500 hover:text-white px-6' onClick={() => { }}>In Progress</Button>
-                    <Button className='ml-2 bg-green-300 text-black hover:bg-green-500 hover:text-white px-6' onClick={() => { }}>Done</Button>
+                <div className='flex items-center justify-left'>
+                    <Button
+                        className={'ml-2 bg-gray-300 text-black hover:bg-gray-500 hover:text-white px-2 py-2'}
+                        onClick={handleBackButtonClick}>
+                        <ArrowLeftIcon className="h-5 w-5" aria-hidden="true" />
+                    </Button>
+                    <Button
+                        className='ml-2 inline-flex w-full justify-center rounded-md hover:ring-2 hover:ring-gray-300 dark:hover:ring-gray-700 bg-gray-300 px-4 py-2 text-sm font-medium text-black shadow-sm hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2'
+                        onClick={() => { }}>
+                        Order
+                        <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                    </Button>
                 </div>
                 <div className='flex items-center justify-left '>
-                    <Button className='ml-2 bg-gray-300 text-black hover:bg-gray-500 hover:text-white px-6' onClick={() => { }}>Order</Button>
-                    <Button className='ml-2 bg-yellow-300 text-black hover:bg-yellow-500 hover:text-white px-6' onClick={() => { }}>Create Issue</Button>
+                    <input className="shadow appearance-none border border-gray-200 dark:border-gray-700 rounded w-full py-1 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline" id="search" type="text" placeholder="Search"></input>
+                    <Button className='ml-2 bg-blue-300 text-black hover:bg-blue-500 hover:text-white px-6' onClick={() => { }}>Search</Button>
                 </div>
             </div>
             {repos.map((repo: any, i) => (
