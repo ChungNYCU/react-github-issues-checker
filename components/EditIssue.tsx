be9d2ed9fb6c6a4ecc05f9a4f85a88ae650addfa
Wei-Che Chung <wchung@seattleu.edu>
2023-02-12 22:31:58 -0800

    Create EditIssue.tsx new version

--- components/EditIssue.tsx
+++ components/EditIssue.tsx
@@ -6,23 +6,42 @@ import { ArrowLeftIcon } from '@heroicons/react/20/solid'
 
 import Button from './Button'
 import { WorkStatus } from '@/modules/WorkStatus'
-import { handleBackButtonClick } from './fetchGitHubApi'
+import { fetchRepoIssue, fetchRepoTitleAndBody, handleBackButtonClick } from './fetchGitHubApi'
 
 type EditIssueProps = {
     username: string;
     reponame: string;
+    issue: string;
+}
+
+type IssueProps = {
+    state: string;
+    title: string;
+    body: string;
 }
 
 const EditIssue = (props: EditIssueProps) => {
 
     const { data: session } = useSession()
     const [token, setToken] = useState<any>('')
-
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [isLoading, setLoading] = useState(false)
 
     useEffect(() => {
         setToken(session?.accessToken)
     }, [])
 
+    useEffect(() => {
+        fetchRepoTitleAndBody(props.username, props.reponame, Number(props.issue), setTitle, setBody, setLoading)
+    }, [])
+
+    const handleTitleChange = (event: any) => {
+        setTitle(event.target.value)
+    }
+    const handleBodyChange = (event: any) => {
+        setBody(event.target.value)
+    }
 
 
     // Handles the submit event on form submit.
@@ -38,8 +57,8 @@ const EditIssue = (props: EditIssueProps) => {
             labels: [event.target.labels.value],
         }
 
-        fetch(`https://api.github.com/repos/${props.username}/${props.reponame}/issues`, {
-            method: 'POST',
+        fetch(`https://api.github.com/repos/${props.username}/${props.reponame}/issues/${props.issue}`, {
+            method: 'PATCH',
             body: JSON.stringify(data),
             headers: {
                 'Authorization': `bearer ${token}`,
@@ -48,20 +67,17 @@ const EditIssue = (props: EditIssueProps) => {
         })
             .then((response) => {
                 response.json()
-                if (response.status === 201) {
-                    alert('Issue creation successfully')
+                if (response.status === 200) {
+                    alert('Issue update successfully')
                     Router.back()
                 } else {
-                    alert(`${response.status} Issue creation failed`)
+                    alert(`${response.status} Issue update failed`)
                 }
             })
-            .then((json) => console.log(json))
             .catch(error => console.error(error))
     }
 
-
     return (
-        // We pass the event to the handleSubmit() function on submit.
         <div>
             <div className='flex items-center justify-left '>
                 <div>
@@ -75,44 +91,41 @@ const EditIssue = (props: EditIssueProps) => {
             <form onSubmit={handleSubmit}>
                 <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Issue title</label>
                 <div className='flex items-center justify-between'>
-
                     <input type="text" id="title" name="title"
                         className="mr-2 block p-2.5 w-5/6 text-sm text-gray-900 bg-gray-50 rounded-lg border 
-                            border-gray-300 focus:ring-blue-500 focus:border-blue-500 
-                            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                            dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Enter issue title"
+                                border-gray-300 focus:ring-blue-500 focus:border-blue-500 
+                                dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                                dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Enter issue title" value={title} onChange={handleTitleChange}
                         required />
-
-                    <select name="labels" id="labels"
+                    <select name="labels" id="labels" required
                         className="block p-2.5 w-1/6 text-sm text-gray-900 bg-gray-50 rounded-lg border 
-                            border-gray-300 focus:ring-blue-500 focus:border-blue-500 
-                            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                            dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                border-gray-300 focus:ring-blue-500 focus:border-blue-500 
+                                dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                                dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="">Task status</option>
                         <option value={WorkStatus.Open}>{WorkStatus.Open}</option>
                         <option value={WorkStatus.InProgress}>{WorkStatus.InProgress}</option>
                         <option value={WorkStatus.Done}>{WorkStatus.Done}</option>
                     </select>
-
                 </div><br />
 
                 <label htmlFor="body" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Issue body</label>
                 <textarea id="body" rows={6} minLength={30}
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border 
-                        border-gray-300 focus:ring-blue-500 focus:border-blue-500 
-                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                        dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Enter issue body"
+                            border-gray-300 focus:ring-blue-500 focus:border-blue-500 
+                            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                            dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter issue body" value={body} onChange={handleBodyChange}
                     required>
                 </textarea><br />
-
                 <div className='flex items-center justify-end'>
                     <Button className='bg-blue-300 text-black hover:bg-blue-500 hover:text-white px-6' onClick={() => { }}>Submit</Button>
                 </div>
             </form>
         </div>
-
     )
+
 }
 
 export default EditIssue
