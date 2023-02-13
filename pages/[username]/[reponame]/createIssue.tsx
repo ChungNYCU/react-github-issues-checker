b939fa85e15b8c5b2deac4904d83d9e8a39dbfce
Wei-Che Chung <wchung@seattleu.edu>
2023-02-12 22:34:01 -0800

    Update createIssue page

--- pages/[username]/[reponame]/createIssue.tsx
+++ pages/[username]/[reponame]/createIssue.tsx
@@ -1,11 +1,7 @@
-import Link from 'next/link'
-
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 
-
-import Button from '@/components/Button'
-import EditIssue from '@/components/EditIssue'
+import CreateIssue from '@/components/CreateIssue'
 
 // Defining an interface for the URL parameters
 interface IParams extends ParsedUrlQuery {
@@ -18,20 +14,18 @@ const createIssue = ({ username, reponame }: InferGetServerSidePropsType<typeof 
         <div>
             <h1 className='mt-10 dark:text-blue-500'>{`Create issue`}</h1>
             <h3 className='mt-5 mb-5'>Repo: {reponame}</h3>
-            <EditIssue
-                username={username}
-                reponame={reponame} />
+            <CreateIssue username={username} reponame={reponame} />
         </div>
 
     )
 }
 
-// A function that fetches the data needed for the component 'Issues'
+// A function that fetches the data needed for the component 'EditIssue'
 export const getServerSideProps: GetServerSideProps = async (context) => {
     // Extracting the parameters 'username' and 'reponame' from the URL query
     const { username, reponame } = context.query as IParams
 
-    // Returning the extracted parameters as props for the component 'Issues'
+    // Returning the extracted parameters as props for the component 'EditIssue'
     return { props: { username, reponame } }
 };
 
